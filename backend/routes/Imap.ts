1010d4eca8088db24060c0aaeed7b0627b8755d9
brenoca <brenondss@gmail.com>
2024-03-08 01:08:08 -0300

    Add: Imap code to get emails info

--- backend/routes/Imap.ts
+++ backend/routes/Imap.ts
@@ -1,16 +1,25 @@
 import { $ } from "bun"
 import express from 'express'
-import getEmails from "../handlers/Imap"
+import getCurrentUser from "../utils/getCurrentUser"
 
 const Imap = express.Router()
 
 Imap.post('/', async (req, res) => {
     try {
-        const {email, password} = req.body
-        if(!email || !password) return res.json({error: "Email or password not provided"})
+        const {email, password, imapServer} = req.body
+        const user = await getCurrentUser()
 
-        const message = await getEmails(email, password)
-        return res.json({...message})
+        if(!email || !password) return res.json({error: "Email or password not provided"})
+        const {stdout, stderr} = (await $`node /home/${user}/traveler-data/travelerAdmin/backend/handlers/getCodeAndVerificationLink.js ${email} ${password} ${imapServer}`)
+
+        const response = stdout.toString()
+        const error = stderr.toString()
+        
+        if(error.length > 0) {
+            return res.json({message: error})
+        }
+        
+        return res.json({message: JSON.parse(response.trim()) })
     } catch(error) {
         return res.json(error)
     }   
